fa1f2a4e58c81fd897029055b09ec8af69db46ee
AliKaledMahmoud <AliKaledMahmoud10@gmail.com>
2026-09-05 15:24:17 +0300

    Add superuser creation command to build script for automated setup

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# أمر إنشاء المسؤول تلقائياً (سيتم يتجاهل الأمر إذا كان الحساب موجوداً بالفعل)
+python manage.py createsuperuser --noinput || true
